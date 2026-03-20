9f4dcb682c35a1ae1d595547510b6d2ed4cf1b87
larissaibanez <larissaibanez.bg028@academico.ifsul.edu.br>
2026-03-20 16:39:14 -0300

    klk

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Buttom, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import {Ionicons} from "@expo/vector-icons";
+
+ 
+const Drawer = createDrawerNavigator();
+function Inicial({navigation}){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> Tela inicial </Text>
+      <Buttom title="Fazer pedido"
+       onPress={() => navigation.navigate("Tela de pedido")}/>
     </View>
   );
 }
 
+function pedido(navigation){
+  return(
+    <View >
+      <Text> Tela de pedido </Text>
+      <Buttom title="Voltar para tela inicial" 
+      onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName= "Tela inicial ">
+          screenOptions={{
+            DrawerStyle: {
+              backgroundColor: "#2d0b57",
+              width: "60%",
+            },
+            DrawerActiveTintColor: "#ff83e0",
+            DrawerActiveBackgroundColor: "#c2c2c2",
+            DrawerInactiveTintColor: "#000000",
+
+            }}
+    <Drawer.Screen 
+      name="Tela inicial" component={Inicial} options={{
+        drawerLabel:"Página no começo",
+        headerShown: true,
+        drawerIcon: ({color, size}) => (
+          <Ionicons name="home" size={size} color={color}/>
+        ),
+      }} />
+      <Drawer.Screen 
+      name="Pedido" component={pedido} options={{
+        drawerIcon: ({color, size}) => (
+          <Ionicons name="home" size={size} color={color}/>
+        ),
+      }} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+}); 
